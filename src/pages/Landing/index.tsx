382cb34495fd414110b1a2a651b7da15c2b813f6
Antônio Almeida <toninetocontato@gmail.com>
2020-08-04 03:24:06 -0300

    Landing page add

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
-import giveClassesIcon from '../../assets/images/icons/study.svg';
-import purpleHeartIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import './styles.css';
 
 function Landing() {
     return (
@@ -25,14 +26,14 @@ function Landing() {
                         Estudar
                     </a>
 
-                    <a href="" className="give-class">
-                        <img src={giveClassesIcon} alt="Estudar"/>
-                        Estudar
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas"/>
+                        Dar aulas
                     </a>
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src="purpleHeartIcon" alt="Coração roxo"/>
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span>
             </div>
         </div>
